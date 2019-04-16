f3f490af219912367dc99b5bde262576c2017010
StevenSymons <symons_steven@hotmail.com>
2019-04-16 23:57:16 +0200

    small update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Header from "./layouts/header";
 import CollapsedComponent from "./CollapsedComponent";
 import Pagination from "./Pagination";
 import {
+  CssBaseline,
   Paper,
   Table,
   TableHead,
   TableBody,
   TableRow,
   TableCell,
-  Collapse
+  Collapse,
+  Typography,
+  CardMedia,
+  Card,
+  CardContent,
+  Button
 } from "@material-ui/core";
 
 const styles = {
@@ -20,7 +26,7 @@ const styles = {
     alignItems: "center"
   },
   Paper: {
-    width: 700,
+    width: 960,
     height: "100%"
   },
   TableRow: {
@@ -49,33 +55,13 @@ class App extends Component {
   };
 
   onChangePage = (e, page) => {
-    const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=HDm4kf9OkZvd3cMbYB5cOjGUsoPGR4fU&size=${
-      this.state.rowsPerPage
-    }&page=${this.state.page}&countryCode=BE`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ events: data._embedded.events, page });
-      })
-      .catch(e => console.log(e));
+    this.setState({ page });
   };
 
   onChangeRowsPerPage = e => {
     e.persist();
     const rowsPerPage = parseInt(e.target.value);
-    const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=HDm4kf9OkZvd3cMbYB5cOjGUsoPGR4fU&size=${rowsPerPage}&page=${
-      this.state.page
-    }&countryCode=BE`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          events: data._embedded.events,
-          pageCount: data.page.totalElements,
-          rowsPerPage
-        });
-      })
-      .catch(e => console.log(e));
+    this.setState({ rowsPerPage });
   };
 
   componentDidMount() {
@@ -96,77 +82,141 @@ class App extends Component {
       .catch(e => console.log(e));
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.rowsPerPage !== this.state.rowsPerPage ||
+      prevState.page !== this.state.page
+    ) {
+      const { page, rowsPerPage } = this.state;
+      const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=HDm4kf9OkZvd3cMbYB5cOjGUsoPGR4fU&size=${rowsPerPage}&page=${page}&countryCode=BE`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            events: data._embedded.events
+          });
+        })
+        .catch(e => console.log(e));
+    }
+  }
+
   render() {
     const { events, isLoading, page, rowsPerPage, pageCount } = this.state;
     return (
-      <div style={styles.Root}>
-        <Paper style={styles.Paper}>
-          <Header />
-          <Table style={styles.Table}>
-            <TableHead>
-              <TableRow>
-                <TableCell>Event</TableCell>
-                <TableCell>Genre</TableCell>
-                <TableCell>Date</TableCell>
-                <TableCell>Price</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {isLoading ? (
+      <Fragment>
+        <CssBaseline />
+        <div style={styles.Root}>
+          <Paper style={styles.Paper}>
+            <Header />
+            <Table>
+              <colgroup>
+                <col style={{ width: "34%" }} />
+                <col style={{ width: "22%" }} />
+                <col style={{ width: "22%" }} />
+                <col style={{ width: "22%" }} />
+              </colgroup>
+              <TableHead>
                 <TableRow>
-                  <TableCell>Loading...</TableCell>
-                  <TableCell>Loading...</TableCell>
-                  <TableCell>Loading...</TableCell>
-                  <TableCell>Loading...</TableCell>
+                  <TableCell>Event</TableCell>
+                  <TableCell>Genre</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Location</TableCell>
                 </TableRow>
-              ) : (
-                events.map(event => {
-                  const {
-                    id,
-                    name,
-                    classifications,
-                    dates,
-                    priceRanges
-                  } = event;
-                  return (
-                    <Fragment key={id}>
-                      <TableRow
-                        hover={true}
-                        style={styles.TableRow}
-                        onClick={() => this.handleClick(event, id)}
-                      >
-                        <TableCell>{name}</TableCell>
-                        <TableCell>{classifications[0].genre.name}</TableCell>
-                        <TableCell>{dates.start.localDate}</TableCell>
-                        <TableCell>
-                          EUR
-                          {priceRanges
-                            ? priceRanges[0].max
-                            : "Price unavailable"}
-                        </TableCell>
-                      </TableRow>
-                      <Collapse
-                        in={this.state[id]}
-                        timeout={{ enter: 0, exit: 0 }}
-                        component={CollapsedComponent}
-                        unmountOnExit
-                      />
-                    </Fragment>
-                  );
-                })
-              )}
-            </TableBody>
-            <Pagination
-              page={page}
-              rowsPerPage={rowsPerPage}
-              pageCount={pageCount}
-              isLoading={isLoading}
-              onChangePage={this.onChangePage}
-              onChangeRowsPerPage={this.onChangeRowsPerPage}
-            />
-          </Table>
-        </Paper>
-      </div>
+              </TableHead>
+              <TableBody>
+                {isLoading ? (
+                  <TableRow>
+                    <TableCell>Loading...</TableCell>
+                    <TableCell>Loading...</TableCell>
+                    <TableCell>Loading...</TableCell>
+                    <TableCell>Loading...</TableCell>
+                  </TableRow>
+                ) : (
+                  events.map(event => {
+                    const {
+                      id,
+                      name,
+                      classifications,
+                      dates,
+                      priceRanges
+                    } = event;
+                    return (
+                      <Fragment key={id}>
+                        <TableRow
+                          hover={true}
+                          style={styles.TableRow}
+                          onClick={() => this.handleClick(event, id)}
+                        >
+                          <TableCell>{name}</TableCell>
+                          <TableCell>{classifications[0].genre.name}</TableCell>
+                          <TableCell>{dates.start.localDate}</TableCell>
+                          <TableCell>
+                            {event._embedded.venues[0].name}
+                          </TableCell>
+                        </TableRow>
+                        <Collapse
+                          in={this.state[id]}
+                          timeout={{ enter: 0, exit: 0 }}
+                          component={CollapsedComponent}
+                          unmountOnExit
+                        >
+                          <Card>
+                            <div>
+                              <CardContent
+                                style={{
+                                  display: "flex",
+                                  flexDirection: "row"
+                                }}
+                              >
+                                <div>
+                                  <CardMedia
+                                    style={{ height: 225, width: 305 }}
+                                    image={event.images[0].url}
+                                  />
+                                </div>
+                                <div>
+                                  <Typography>{name}</Typography>
+                                  <Typography>
+                                    {dates.start.localDate}
+                                  </Typography>
+                                  <Typography>
+                                    Price:{" "}
+                                    {priceRanges
+                                      ? priceRanges[0].max
+                                      : "No price available yet."}
+                                  </Typography>
+                                  <Typography>
+                                    Location: {event._embedded.venues[0].name}
+                                  </Typography>
+                                  <Button
+                                    href="http://www.google.com"
+                                    variant="contained"
+                                    color="primary"
+                                  >
+                                    Check it out!
+                                  </Button>
+                                </div>
+                              </CardContent>
+                            </div>
+                          </Card>
+                        </Collapse>
+                      </Fragment>
+                    );
+                  })
+                )}
+              </TableBody>
+              <Pagination
+                page={page}
+                rowsPerPage={rowsPerPage}
+                pageCount={pageCount}
+                isLoading={isLoading}
+                onChangePage={this.onChangePage}
+                onChangeRowsPerPage={this.onChangeRowsPerPage}
+              />
+            </Table>
+          </Paper>
+        </div>
+      </Fragment>
     );
   }
 }
--- src/CollapsedComponent.js
+++ src/CollapsedComponent.js
@@ -1,30 +1,9 @@
 import React from "react";
-import { Card, CardContent, Typography, CardMedia } from "@material-ui/core";
 
 const CollapsedComponent = props => {
   return (
     <tr>
-      <td colSpan={4}>
-        <Card>
-          <div>
-            <CardContent>
-              <Typography component="h5" variant="h5">
-                Live From Space
-              </Typography>
-              <Typography variant="subtitle1" color="textSecondary">
-                Mac Miller
-              </Typography>
-            </CardContent>
-          </div>
-          <CardMedia
-            style={{ height: 225, width: 305 }}
-            image={
-              "https://s1.ticketm.net/dam/a/6e6/36cc1651-d377-4362-812b-e6865ea106e6_942821_CUSTOM.jpg"
-            }
-            title="Live from space album cover"
-          />
-        </Card>
-      </td>
+      <td colSpan={4}>{props.children}</td>
     </tr>
   );
 };
